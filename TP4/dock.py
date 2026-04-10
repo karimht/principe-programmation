0d8fcb3c434be7b261a668ef5af92861f714fbc7
Karim <Karimmahtout9@gmail.com>
2026-04-10 10:16:24 +0200

    Ajout du ReadMe

--- TP4/dock.py
+++ TP4/dock.py
@@ -1,17 +1,17 @@
-#1. On utilise une image Python officielle
+# 1. Image Python officielle
 FROM python:3.12-slim
 
-#2. On définit le répertoire de travail
+# 2. Répertoire de travail
 WORKDIR /app
 
-#3. On installe Flask
+# 3. Installation de Flask
 RUN pip install flask
 
-#4. On copie le fichier app.py dans l'image
-COPY app.py
+# 4. Copier le fichier app.py
+COPY app.py .
 
-#5. On documente le port utilisé
+# 5. Exposer le port
 EXPOSE 5000
 
-#6. On définit la commande qui démare l'application
-CMD["python","app.py"]
+# 6. Lancer l'application
+CMD ["python", "app.py"]
